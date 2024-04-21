596b06f6021613d16594c0feea2f96c13e3a8a4c
ValentinooooMayoraaaal <151012344+ValentinooooMayoraaaal@users.noreply.github.com>
2024-04-21 15:06:07 +0200

    creation_de_col

--- fonctions.c
+++ fonctions.c
@@ -1,13 +1,17 @@
 //
 // Created by tombo on 27/03/2024.
 //
-Column *create_column(char *title) {
-    Column *colonne = malloc(sizeof(Column));
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    colonne->titre = strdup(title);
+Column *create_column(char *title) {
+    Column *colonne = (colonne*) malloc(sizeof(Column));
+
+    colonne->titre = title;
     colonne->donnees = NULL; // pointeur sur le tableau
     colonne->taille_physique = 256;
     colonne->taille_logique = 0;
-
+    free(colonne);
     return colonne;
 }

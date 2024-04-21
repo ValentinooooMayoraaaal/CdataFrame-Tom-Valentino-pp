d8f1f0a0e3621849e5d5d74f143d83f3570c16e5
Valentino <valkoush@gmail.com>
2024-04-21 16:38:17 +0200

    insert_value_apparement_fonctionnelle

--- main.c
+++ main.c
@@ -6,7 +6,11 @@
 #include "fonctions.h"
 int main() {
     Column *mycol = create_column("My column");
-
+    int val = 5;
+    if (insert_value(mycol, val))
+        printf("Valeur ajoutee avec succes a ma colonne\n");
+    else
+        printf("Erreur lors de l'ajout de la valeur a ma colonne\n");
     return 0;
 }
 

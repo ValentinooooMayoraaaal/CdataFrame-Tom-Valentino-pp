21cb1a6be7528829ca0b7c3fce9a4966e52b9fa3
Valentino <valkoush@gmail.com>
2024-04-21 14:59:11 +0200

    fonction_creer_colonne

--- fonctions.c
+++ fonctions.c
@@ -1,3 +1,13 @@
 //
 // Created by tombo on 27/03/2024.
 //
+Column *create_column(char *title) {
+    Column *colonne = malloc(sizeof(Column));
+
+    colonne->titre = strdup(title);
+    colonne->donnees = NULL; // pointeur sur le tableau
+    colonne->taille_physique = 256;
+    colonne->taille_logique = 0;
+
+    return colonne;
+}
